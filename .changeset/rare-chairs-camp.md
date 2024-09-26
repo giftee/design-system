f43678fa9b36797810b496f5c5432ecbdcf14949
egurinko <egctoru@gmail.com>
2024-09-27 01:48:36 +0900

    enable to use rows and cols in textarea of Textfield

---
+++ .changeset/rare-chairs-camp.md
@@ -0,0 +1,5 @@
+---
+"@giftee/abukuma-css": patch
+---
+
+[change:Textfield] textarea 利用時に rows, cols 指定を可能とした
--- packages/css/src/components/textfield/stories/Type.stories.ts
+++ packages/css/src/components/textfield/stories/Type.stories.ts
@@ -25,15 +25,19 @@ export const Type: Story = {
   </div>
   <div class="ab-Textfield ab-mb-8">
     <label for="textarea" class="ab-Textfield-label">
-      テキストエリア
+      テキストエリア。rows と cols を指定可能です。
       ${args.required ? `<div class="ab-StatusLabel">必須</div>` : ''}
     </label>
     <textarea
       id="textarea"
       placeholder="textarea"
       name="field"
-      class="ab-Textfield-input"
-    ></textarea>
+      class="ab-Textfield-textarea"
+      rows="5"
+      cols="15"
+    >
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </textarea>
     <div class="ab-Textfield-helptext">textarea の Textfield</div>
   </div>
 </div>
