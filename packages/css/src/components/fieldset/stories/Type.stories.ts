72ac1fcafa409b88f38c9c976c4ce6010d36cd00
egurinko <egctoru@gmail.com>
2025-05-16 18:34:27 +0900

    Separate Fieldset story to direction and type

--- packages/css/src/components/fieldset/stories/Type.stories.ts
+++ packages/css/src/components/fieldset/stories/Type.stories.ts
@@ -2,16 +2,15 @@ import { meta, createComponent, Story } from './shared';
 
 export default {
   ...meta,
-  title: 'Component/Fieldset/Direction',
+  title: 'Component/Fieldset/Type',
 };
 
-export const Direction: Story = {
+export const Type: Story = {
   render: (args) => `
 <div class="ab-m-4">
   ${createComponent({
     ...args,
-    legend: 'Horizontal',
-    direction: 'horizontal',
+    legend: 'RadioGroup',
     children: `
       <div class="ab-Radio-wrapper position-right">
         <label for="yes" class="ab-Radio-label">Yes</label>
@@ -46,34 +45,32 @@ export const Direction: Story = {
 <div class="ab-m-4">
   ${createComponent({
     ...args,
-    legend: 'Vertical',
-    direction: 'vertical',
+    legend: 'CheckboxGroup',
     children: `
-      <div class="ab-Radio-wrapper position-right">
-        <label for="yes" class="ab-Radio-label">Yes</label>
-        <div class="ab-Radio">
+      <div class="ab-Checkbox-wrapper position-right">
+        <label for="hoge" class="ab-Checkbox-label">Hoge</label>
+        <div class="ab-Checkbox">
           <input
-            id="yes"
-            name="radio-sample"
-            class="ab-Radio-input"
-            type="radio"
-            checked="true"
-            value="1"
-          />
-          <span class="ab-Radio-radio"></span>
-        </div>
-      </div>
-      <div class="ab-Radio-wrapper position-right">
-        <label for="no" class="ab-Radio-label">No</label>
-        <div class="ab-Radio">
-          <input
-            id="no"
-            name="radio-sample"
-            class="ab-Radio-input"
-            type="radio"
+            id="hoge"
+            name="checkbox-sample"
+            class="ab-Checkbox-input"
+            type="checkbox"
             value="0"
           />
-          <span class="ab-Radio-radio"></span>
+          <span class="ab-Checkbox-box"></span>
+        </div>
+      </div>
+      <div class="ab-Checkbox-wrapper position-right">
+        <label for="fuga" class="ab-Checkbox-label">Fuga</label>
+        <div class="ab-Checkbox">
+          <input
+            id="fuga"
+            name="checkbox-sample"
+            class="ab-Checkbox-input"
+            type="checkbox"
+            value="1"
+          />
+          <span class="ab-Checkbox-box"></span>
         </div>
       </div>
     `,
