519944a91a96ce3fd12491979d4956a027770d72
ishikawamitsugi <m-ishikawa@giftee.co>
2025-05-20 10:16:44 +0900

    add Stepper component

---
+++ packages/react/src/components/stepper/Index.tsx
@@ -0,0 +1,62 @@
+import { forwardRef } from 'react';
+import { classNames } from '@/utils/classNames';
+import type { ComponentPropsWithoutRef, ElementRef } from 'react';
+
+export type StepperProps = ComponentPropsWithoutRef<'ol'> & {
+  currentStep: number;
+  steps: string[];
+  direction?: 'horizontal' | 'vertical';
+  size?: 'small' | 'medium';
+};
+
+export const Stepper = forwardRef<ElementRef<'ol'>, StepperProps>(
+  (
+    {
+      currentStep,
+      steps,
+      direction = 'horizontal',
+      size = 'medium',
+      className,
+      ...rest
+    },
+    forwardedRef,
+  ) => {
+
+    const classes = classNames(
+      'ab-Stepper',
+      `ab-Stepper-${direction}`,
+      `ab-Stepper-${size}`
+    );
+
+    return (
+      <ol className={classes} ref={forwardedRef} {...rest}>
+        {steps.map((label, i) => {
+          const status =
+          i < currentStep
+              ? 'passed'
+              : i === currentStep
+              ? 'current'
+              : undefined;
+
+          return (
+            <li
+              key={label}
+              className={classNames(
+                'ab-Stepper-item',
+                status && `ab-Stepper-item-${status}`,
+              )}
+              aria-current={status === 'current' ? 'step' : undefined}
+            >
+              <div className="ab-Stepper-marker" aria-hidden={status === 'passed'}>
+              {size !== 'small' && status !== 'passed' ? i + 1 : null}
+              </div>
+              <div className="ab-Stepper-label">{label}</div>
+            </li>
+          );
+        })}
+      </ol>
+    );
+  },
+);
+
+Stepper.displayName = 'Stepper';
---
+++ packages/react/src/components/stepper/Stepper.stories.tsx
@@ -0,0 +1,92 @@
+import type { StepperProps } from '@/index';
+import { Stepper } from '@/index';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Stepper',
+  tags: ['autodocs'],
+  component: Stepper,
+  args: {
+    currentStep: 1,
+    steps: [
+      'メールアドレス確認',
+      '認証コード入力',
+      'パスワード設定',
+      '企業情報入力',
+      '登録内容確認',
+    ],
+    direction: 'horizontal',
+    size: 'medium',
+  },
+  argTypes: {
+    currentStep: {
+      control: { type: 'number', min: 0 },
+      description: '現在のステップ（0始まり）',
+    },
+    steps: {
+      control: false,
+      description: 'ステップラベルの配列',
+    },
+    direction: {
+      control: { type: 'select' },
+      options: ['horizontal', 'vertical'],
+      table: {
+        defaultValue: {
+          summary: 'horizontal',
+        },
+      },
+      description: 'ステッパーの方向（横並び／縦並び）',
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium'],
+      table: {
+        defaultValue: {
+          summary: 'medium',
+        },
+      },
+      description: 'ステッパーのサイズ',
+    },
+  },
+} satisfies Meta<typeof Stepper>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  render: (args: StepperProps) => (
+    <div className="ab-m-8">
+      <Stepper {...args} />
+    </div>
+  ),
+};
+
+export const Size: Story = {
+  render: (args: StepperProps) => (
+    <div className="ab-flex ab-gap-8">
+      <div>
+        <p className="ab-mb-2">small</p>
+        <Stepper {...args} size="small" />
+      </div>
+      <div>
+        <p className="ab-mb-2">medium</p>
+        <Stepper {...args} size="medium" />
+      </div>
+    </div>
+  ),
+};
+
+export const Direction: Story = {
+  render: (args: StepperProps) => (
+    <div className="ab-flex ab-gap-8">
+      <div>
+        <p className="ab-mb-2">Horizontal</p>
+        <Stepper {...args} direction="horizontal" />
+      </div>
+      <div>
+        <p className="ab-mb-2">Vertical</p>
+        <Stepper {...args} direction="vertical" />
+      </div>
+    </div>
+  ),
+};
