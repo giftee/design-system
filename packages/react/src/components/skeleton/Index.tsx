e60a91bcb3701c2d9ff9741af48b80ba49a9a0a3
aidyak <yusei.kaida@giftee.co>
2026-01-15 19:46:34 +0900

    chore: JSDocが抜けていたので追加した

--- packages/react/src/components/skeleton/Index.tsx
+++ packages/react/src/components/skeleton/Index.tsx
@@ -3,10 +3,11 @@ import { classNames } from '@/utils/classNames';
 import type { ComponentPropsWithoutRef, ElementRef } from 'react';
 
 export type SkeletonProps = ComponentPropsWithoutRef<'span'> & {
+  /** 形 @default 'text' */
   variant?: 'text' | 'circular' | 'rectangular';
-
+  /** 幅 */
   width?: string | number;
-
+  /** 高さ */
   height?: string | number;
 };
 
