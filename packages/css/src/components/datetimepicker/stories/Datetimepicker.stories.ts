73ddfbe85257ce010591a2e4228febae2cedf0fd
egurinko <egctoru@gmail.com>
2026-03-15 17:05:59 +0900

    add aria-describedby / aria-errormessage and relate with help-text and error message in Datepicker/Datetimepicker

--- packages/css/src/components/datetimepicker/stories/Datetimepicker.stories.ts
+++ packages/css/src/components/datetimepicker/stories/Datetimepicker.stories.ts
@@ -12,64 +12,108 @@ export const State: Story = {
 <div id="default" class="ab-Textfield ab-mb-8">
   <label for="default" class="ab-Textfield-label">
     Default
-    ${args.required ? `<div class="ab-StatusLabel">必須</div>` : ''}
+    ${args.required ? `<span class="ab-StatusLabel">必須</span>` : ''}
   </label>
   <span class="ab-DateTimePicker">
-    <input type="datetime-local" name="default" id="default" class="ab-DateTimePicker-input" />
+    <input
+      type="datetime-local"
+      name="default"
+      id="default"
+      class="ab-DateTimePicker-input"
+      ${args.required ? 'required' : ''}
+      aria-describedby="default-helptext"
+    />
   </span>
-  <div class="ab-Textfield-helptext">default</div>
+  <div id="default-helptext" class="ab-Textfield-helptext">default</div>
 </div>
 <div id="hover" class="ab-Textfield ab-mb-8">
   <label for="hover" class="ab-Textfield-label">
     Hover
-    ${args.required ? `<div class="ab-StatusLabel">必須</div>` : ''}
+    ${args.required ? `<span class="ab-StatusLabel">必須</span>` : ''}
   </label>
   <span class="ab-DateTimePicker">
-    <input type="datetime-local" name="hover" id="hover" class="ab-DateTimePicker-input" />
+    <input
+      type="datetime-local"
+      name="hover"
+      id="hover"
+      class="ab-DateTimePicker-input"
+      ${args.required ? 'required' : ''}
+      aria-describedby="hover-helptext"
+    />
   </span>
-  <div class="ab-Textfield-helptext">hover</div>
+  <div id="hover-helptext" class="ab-Textfield-helptext">hover</div>
 </div>
 <div id="active" class="ab-Textfield ab-mb-8">
   <label for="active" class="ab-Textfield-label">
     Active
-    ${args.required ? `<div class="ab-StatusLabel">必須</div>` : ''}
+    ${args.required ? `<span class="ab-StatusLabel">必須</span>` : ''}
   </label>
   <span class="ab-DateTimePicker">
-    <input type="datetime-local" name="active" id="active" class="ab-DateTimePicker-input" />
+    <input
+      type="datetime-local"
+      name="active"
+      id="active"
+      class="ab-DateTimePicker-input"
+      ${args.required ? 'required' : ''}
+      aria-describedby="active-helptext"
+    />
   </span>
-  <div class="ab-Textfield-helptext">active</div>
+  <div id="active-helptext" class="ab-Textfield-helptext">active</div>
 </div>
 <div id="focus" class="ab-Textfield ab-mb-8">
   <label for="focus" class="ab-Textfield-label">
     Focus
-    ${args.required ? `<div class="ab-StatusLabel">必須</div>` : ''}
+    ${args.required ? `<span class="ab-StatusLabel">必須</span>` : ''}
   </label>
   <span class="ab-DateTimePicker">
-    <input type="datetime-local" name="focus" id="focus" class="ab-DateTimePicker-input" />
+    <input
+      type="datetime-local"
+      name="focus"
+      id="focus"
+      class="ab-DateTimePicker-input"
+      ${args.required ? 'required' : ''}
+      aria-describedby="focus-helptext"
+    />
   </span>
-  <div class="ab-Textfield-helptext">focus</div>
+  <div id="focus-helptext" class="ab-Textfield-helptext">focus</div>
 </div>
 <div class="ab-Textfield ab-mb-8">
   <label for="disabled" class="ab-Textfield-label">
     Disabled
-    ${args.required ? `<div class="ab-StatusLabel">必須</div>` : ''}
+    ${args.required ? `<span class="ab-StatusLabel">必須</span>` : ''}
   </label>
   <span class="ab-DateTimePicker">
-    <input type="datetime-local" name="disabled" id="disabled" disabled class="ab-DateTimePicker-input" />
+    <input
+      type="datetime-local"
+      name="disabled"
+      id="disabled"
+      disabled
+      class="ab-DateTimePicker-input"
+      ${args.required ? 'required' : ''}
+      aria-describedby="disabled-helptext"
+    />
   </span>
-  <div class="ab-Textfield-helptext">disabled</div>
+  <div id="disabled-helptext" class="ab-Textfield-helptext">disabled</div>
 </div>
 <div id="error" class="ab-Textfield is-error ab-mb-8">
   <label for="error" class="ab-Textfield-label">
     Error
-    ${args.required ? `<div class="ab-StatusLabel">必須</div>` : ''}
+    ${args.required ? `<span class="ab-StatusLabel">必須</span>` : ''}
   </label>
   <span class="ab-DateTimePicker">
-    <input type="datetime-local" name="error" id="error" class="ab-DateTimePicker-input" />
+    <input
+      type="datetime-local"
+      name="error"
+      id="error"
+      class="ab-DateTimePicker-input"
+      ${args.required ? 'required' : ''}
+      aria-invalid="true"
+      aria-describedby="error-message error-helptext"
+      aria-errormessage="error-message"
+    />
   </span>
-  <div class="ab-Textfield-error-message">error</div>
-  <div class="ab-Textfield-error-message">error</div>
-  <div class="ab-Textfield-helptext">helptext</div>
+  <div id="error-message" class="ab-Textfield-error-message">error</div>
+  <div id="error-helptext" class="ab-Textfield-helptext">helptext</div>
 </div>
   `;
   },
