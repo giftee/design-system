9add45b4c3a8bd082edf333bbe6a688a74197379
egurinko <egctoru@gmail.com>
2026-03-16 18:15:55 +0900

    remove role menu and add aria-expanded

--- packages/css/src/components/popover/stories/Direction.stories.ts
+++ packages/css/src/components/popover/stories/Direction.stories.ts
@@ -1,4 +1,4 @@
-import { meta } from './shared';
+import { createComponent, interactionScript, meta } from './shared';
 import type { Story } from './shared';
 
 export default {
@@ -11,166 +11,77 @@ export const Direction: Story = {
     return `
 <h4 class="ab-text-headline-m ab-mb-30">PopoverTop</h4>
 <div class="ab-flex ab-flex-justify-around">
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-top">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">MenuItem2</li>
-      </ul>
-    </div>
-  </div>
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-top-start">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">MenuItem2</li>
-      </ul>
-    </div>
-  </div>
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-top-end">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">MenuItem2</li>
-      </ul>
-    </div>
-  </div>
+  ${createComponent({
+    buttonId: 'hover',
+    popoverId: 'popover-top',
+    positionClass: 'ab-Popover-top',
+  })}
+  ${createComponent({
+    buttonId: 'active',
+    popoverId: 'popover-top-start',
+    positionClass: 'ab-Popover-top-start',
+  })}
+  ${createComponent({
+    buttonId: 'focus',
+    popoverId: 'popover-top-end',
+    positionClass: 'ab-Popover-top-end',
+  })}
 </div>
 <h4 class="ab-text-headline-m">PopoverBottom</h4>
 <div class="ab-flex ab-flex-justify-around">
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-bottom">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">MenuItem2</li>
-      </ul>
-    </div>
-  </div>
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-bottom-start">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">MenuItem2</li>
-      </ul>
-    </div>
-  </div>
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-bottom-end">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">MenuItem2</li>
-      </ul>
-    </div>
-  </div>
+  ${createComponent({
+    buttonId: 'popover-bottom-button',
+    popoverId: 'popover-bottom',
+    positionClass: 'ab-Popover-bottom',
+  })}
+  ${createComponent({
+    buttonId: 'popover-bottom-start-button',
+    popoverId: 'popover-bottom-start',
+    positionClass: 'ab-Popover-bottom-start',
+  })}
+  ${createComponent({
+    buttonId: 'popover-bottom-end-button',
+    popoverId: 'popover-bottom-end',
+    positionClass: 'ab-Popover-bottom-end',
+  })}
 </div>
 <h4 class="ab-text-headline-m ab-mt-30 ab-mb-10">PopoverLeft</h4>
 <div class="ab-flex ab-flex-justify-around">
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-left">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">MenuItem2</li>
-      </ul>
-    </div>
-  </div>
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-left-start">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">MenuItem2</li>
-      </ul>
-    </div>
-  </div>
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-left-end">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">MenuItem2</li>
-      </ul>
-    </div>
-  </div>
+  ${createComponent({
+    buttonId: 'popover-left-button',
+    popoverId: 'popover-left',
+    positionClass: 'ab-Popover-left',
+  })}
+  ${createComponent({
+    buttonId: 'popover-left-start-button',
+    popoverId: 'popover-left-start',
+    positionClass: 'ab-Popover-left-start',
+  })}
+  ${createComponent({
+    buttonId: 'popover-left-end-button',
+    popoverId: 'popover-left-end',
+    positionClass: 'ab-Popover-left-end',
+  })}
 </div>
 <h4 class="ab-text-headline-m ab-my-20">PopoverRight</h4>
 <div class="ab-flex ab-flex-justify-around">
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-right">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem2
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-right-start">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem2
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div class="ab-position-relative">
-    <button class="ab-Button">Popover Source</button>
-    <div class="ab-Popover ab-Popover-right-end">
-      <ul class="ab-Menu ab-Menu-small" role="menu">
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">
-          MenuItem1
-        </li>
-        <li class="ab-Menu-item" role="menuitem" tabindex="0">MenuItem2</li>
-      </ul>
-    </div>
-  </div>
+  ${createComponent({
+    buttonId: 'popover-right-button',
+    popoverId: 'popover-right',
+    positionClass: 'ab-Popover-right',
+  })}
+  ${createComponent({
+    buttonId: 'popover-right-start-button',
+    popoverId: 'popover-right-start',
+    positionClass: 'ab-Popover-right-start',
+  })}
+  ${createComponent({
+    buttonId: 'popover-right-end-button',
+    popoverId: 'popover-right-end',
+    positionClass: 'ab-Popover-right-end',
+  })}
 </div>
-<script>
-  document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.ab-Button');
-
-    buttons.forEach((button) => {
-      const popover = button.parentElement.querySelector('.ab-Popover');
-      if (!popover) return;
-
-      button.addEventListener('click', (e) => {
-        popover.classList.toggle('is-open');
-      });
-    });
-  });
-</script>
+${interactionScript}
   `;
   },
   parameters: {
